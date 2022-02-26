d8db5632c5ff8c4e68ffd83476c5ba36d6aec667
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2022-02-26 04:38:12 -0500

    Api stuff donezoz

---
+++ app/api_parsing.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+import json
+
+def get_holidays(day, month, country):
+    result = []
+
+    payload  = {
+    "api_key": "e9c15099a5eb94fe889eaca1c70c6fae9eb5634a",
+    "country" : country,
+    "year" : 2022,
+    "day" : day,
+    "month" : month
+    }
+
+    url = "https://calendarific.com/api/v2/holidays" 
+    try:  
+        r = requests.get(url,params=payload,timeout=0.8)
+        data = r.json()
+    except:
+        data = {
+                "response":{
+                    "holidays": []
+                    }
+                }
+
+    for i in data["response"]["holidays"]:
+        holiday_name = i["name"] 
+        result.append(holiday_name)
+    
+    print("done " + country)
+    return result
+
+def get_holiday_data(day, month):
+    d = {}
+    country_codes = [["IN","India"], ["CA", "Canda"], ["UK","United Kingdom"], ["CN", "China"], ["DE", "Germany"], ["JP", "Japan"], ["KR", "Korea"], ["FR", "France"],["MX", "Mexico"], ["AU", "Australia"], ["BR", "Brazil"]]
+    for i in country_codes:
+        try:
+            l = get_holidays(day, month, i[0])
+            if len(l) > 0:
+                d[i[1]] = l
+        except:
+            pass
+    return d
+
+for i in range(1, 12):
+    print(get_holiday_data(10,i))
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, flash , redirect , url_for ,request
+from api_parsing import get_holidays
 
 @app.route('/')
 def index():
