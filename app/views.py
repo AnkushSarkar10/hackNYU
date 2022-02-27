13034ef06046a8646f6f131c9088adef68aa7f9c
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2022-02-27 01:22:53 -0500

    new_results

--- app/views.py
+++ app/views.py
@@ -18,8 +18,8 @@ def results():
         day = date_obj.day
         month = date_obj.month
 
-        # data = api_parsing.get_holiday_data(day, month)
-        data = {'India': ['Mahatma Gandhi Jayanti', 'Maha Saptami'], 'China': ['National Day Golden Week holiday'], 'Germany': ['Harvest Festival'], 'Australia': ['Daylight Saving Time starts']}
+        data = api_parsing.get_holiday_data(day, month)
+        # data = {'India': ['Mahatma Gandhi Jayanti', 'Maha Saptami'], 'China': ['National Day Golden Week holiday'], 'Germany': ['Harvest Festival'], 'Australia': ['Daylight Saving Time starts']}
         return render_template("html/new_results.html",data=data)
 
 
